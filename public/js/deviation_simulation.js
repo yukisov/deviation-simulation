fc681f90a85d4f70412e3aa64455d002dfa4a51d
yuki <yukisov@gmail.com>
2015-08-07 22:54:42 +0900

    Add base files

---
+++ public/js/deviation_simulation.js
@@ -0,0 +1,23 @@
+(function(global){
+  "use strict";
+
+  global.app = global.app || {};
+
+  //---------------------------
+  //
+  //---------------------------
+  global.app.xxxx = (function(/*global*/) {
+
+  })(global);
+
+
+  //------------
+  // Main
+  //------------
+  $(function(){
+
+    //global.app.form.setHandlers();
+
+  });
+
+})((typeof window === 'object' && window) || global);
